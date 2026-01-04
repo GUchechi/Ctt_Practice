db0b08b142aa30204203b3830e5070950104b681
GUchechi <godswilluchendu7@gmail.com>
2026-01-05 00:40:44 +0100

    numeric functions

---
+++ numericFunctions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main()
+{
+   
+
+    cout << sqrt(25) << endl;
+    cout << pow(2, 8) << endl;
+    cout << remainder(10.5, 3.0) << endl;
+    cout << fmod(10.5, 3.0) << endl;
+    cout << fmin(10.5, 3.0) << endl;
+    cout << ceil(10.5) << endl;
+    cout << floor(10.5) << endl;
+    cout << round(10.5) << endl;
+    cout << trunc(10.5) << endl;
+
+    return 0;
+}

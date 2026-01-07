b81675befe9220a5cd993a52fae80a9b094398d0
GUchechi <godswilluchendu7@gmail.com>
2026-01-07 18:00:00 +0100

    refactoring

--- conditionalOperator.cpp
+++ conditionalOperator.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+
+    // int a = 10;
+    // int b = 20;
+    // int max = (a > b) ? a : b;
+    // cout << "Maximum of " << a << " and " << b << " is: " << max << endl;
+
     int answer = 11;
     cout << "Guess the number (between 1 and 20): ";
     int guess;

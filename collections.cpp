e81546d2b4dadf0fc69db171b712a29a2816452a
GUchechi <godswilluchendu7@gmail.com>
2026-01-10 00:32:01 +0100

    refactor

---
+++ collections.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int guesses[20];
+    std::cin >> guesses[0];
+    std::cout << "You entered: " << guesses[0] << std::endl;
+
+    
+
+    return 0;
+}

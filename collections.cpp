b7fff95a99f6a43adbb7365edb1816286c8c2dbb
GUchechi <godswilluchendu7@gmail.com>
2026-01-10 01:08:04 +0100

    refactor

--- collections.cpp
+++ collections.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void print_array(int arr[], int size) {
-    for (int i = 0; i < size; i++ ){
+void print_array(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         cout << arr[i] << "\t";
     }
 }
 
-int main() {
-    
-    int guesses[] = {10, 50, 100, 150, 200, 250};
-    int size = sizeof(guesses) / sizeof(guesses[0]);
-
+int main()
+{
+    const int SIZE = 6;
+    int guesses[SIZE];
     // int size = sizeof(guesses) / sizeof(guesses[0]);
-    // for (int i = 0; i < size; i++ ){
-    //     cout << guesses[i] << endl;
-    // }
-    print_array(guesses, size);
-    
+
+    for (int i = 0; i < SIZE; i++)
+    {
+      if  (cin >> guesses[i]) {
+        // Successfully read an integer
+      } else {
+        // Failed to read an integer
+        cout << "Invalid input. Please enter integers only." << endl;
+        return 1; // Exit the program with an error code
+      }
+    }
+
+    print_array(guesses, SIZE);
 
     return 0;
 }

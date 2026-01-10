e6c7421c19f37d56fe4fde7c8366c6f745f6998e
GUchechi <godswilluchendu7@gmail.com>
2026-01-10 03:16:41 +0100

    added vector

--- vectors.cpp
+++ vectors.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 #include <vector>
 
-void print_vector (vector <int> &data) {
+void print_vector(vector<int> &data)
+{
     data.push_back(4); // Modifying the vector by adding an element
 
-    for ( size_t i = 0; i < data.size(); i++) {
+    for (size_t i = 0; i < data.size(); i++)
+    {
         cout << data[i] << "\t";
     }
 }
 
-int main() {
-    
-    vector<int> data = {1,2,3};
-    print_vector(data); 
+int main()
+{
+
+    vector<int> data = {1, 2, 3};
+    print_vector(data);
 
     return 0;
 }

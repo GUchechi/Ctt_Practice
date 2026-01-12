aafd53b7dbbf104efc29ff82be4cb82c8284dc52
GUchechi <godswilluchendu7@gmail.com>
2026-01-12 23:25:34 +0100

    multi compilation

---
+++ MultifileCompilation/math_stuff.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math_utils.h"
+using namespace std;
+
+int main()
+{
+
+    cout << pow(3, 3) << endl; // 27
+    cout << pow(3) << endl;    // 9 (default power is 2)
+
+    return 0;
+}
